3dd16709697cfff24c62f3498241819968774207
patrickTomic <pat12tomic@gmail.com>
2025-07-02 06:50:52 -0400

    getPlanet component has errors with texture. may switch the a shaders version

---
+++ src/components/getPlanet.ts
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+ import vertexShader from '../shaders/vertex.glsl'
+import fragmentShader from '../shaders/fragment.glsl'
+ 
+const geo = new THREE.IcosahedronGeometry(1, 6);
+export default function getPlanet(src: any, distance: any,  size: number,children: any[] = []  ){
+  const group = new THREE.Group()
+ const map = new THREE.TextureLoader().load(src)
+ const mat = new THREE.MeshStandardMaterial({
+      map,
+    });
+    const sphere = new THREE.Mesh(geo, mat)
+const angle = 50 //Math.random() * Math.PI * 2;
+console.log(src)
+children.forEach((child) => {
+  child.position.x = Math.cos(angle)* distance
+child.positon.z = Math.sin(angle) *distance
+group.add(child)
+})
+sphere.position.x = Math.cos(angle)* distance
+
+sphere.scale.setScalar(size)
+group.add(sphere)
+return group
+}
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ import fragmentShader from './shaders/fragment.glsl'
 import sun from './assets/sun.jpg'
 import earth from './assets/earth.jpg'
 import moon  from './assets/moon.jpg'
+import mercury from './assets/mercury.jpg'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import getPlanet from './components/getPlanet'
 const scene =  new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.1,1000);
 
@@ -16,19 +19,25 @@ const renderer = new THREE.WebGLRenderer({
     antialias:true,
    
 })
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update()
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
+ 
+
 //document.body.appendChild(renderer.domElement)
 
 //add moon 
-/*
+ 
 const moonGroup = new THREE.Group();
-scene.add(moonGroup);
+ 
 
 
 
-const moonMesh = new THREE.Mesh(new THREE.SphereGeometry(5,50,50),
+const moonMesh = new THREE.Mesh(new THREE.SphereGeometry(5,10,50),
  new THREE.ShaderMaterial({
  vertexShader: vertexShader,
  fragmentShader,
@@ -39,10 +48,10 @@ const moonMesh = new THREE.Mesh(new THREE.SphereGeometry(5,50,50),
  }
 }))
 
-moonMesh.position.set(10,0,0)
-moonMesh.scale.setScalar(0.27)
+moonMesh.position.set(2,0,0)
+moonMesh.scale.setScalar(0.05)
 moonGroup.add(moonMesh)
-*/
+ 
 const sunSphere = new THREE.Mesh(new THREE.SphereGeometry(5,100,100), new THREE.ShaderMaterial({
 vertexShader: sunVertex,
 fragmentShader: sunFragment,
@@ -58,11 +67,11 @@ const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial)
 const solarSystem = new THREE.Group()
 
 scene.add(solarSystem)
-solarSystem.add(sunSphere)
+solarSystem.add(sunSphere) 
 
 const earthGroup = new THREE.Group()
 
-/*const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50),
+ const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50),
  new THREE.ShaderMaterial({
  vertexShader: vertexShader,
  fragmentShader,
@@ -72,22 +81,30 @@ const earthGroup = new THREE.Group()
     }
  }
 }))
-scene.add(earthGroup)
+const angle = Math.random() * Math.PI * 2;
+earthGroup.position.x = Math.cos(angle) * 2 
+earthGroup.position.z = Math.sin(angle) * 15;
+sphere.scale.setScalar(0.225)
 earthGroup.add(sphere)
-*/
+earthGroup.add(moonGroup)
+solarSystem.add(earthGroup)
 camera.position.z = 20
 
+//add mercury
+const mercur = getPlanet('./assets/mercury.jpg', 10.25 ,0.2 )
+ mercur.position.z = Math.sin(angle) * 10;
+solarSystem.add(mercur)
 
 function animate () {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
  
  scene.rotation.x = Math.PI/6
- /*
+   earthGroup.rotation.x = 0.01
     sphere.rotation.y += 0.001
 
 moonGroup.rotation.y +=0.006 
-*/
+ 
 }
 animate()
 
